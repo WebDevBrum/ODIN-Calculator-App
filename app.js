cada3b7440a77cb70c671048dbbf88cfb119d50c
Scott Davies <scottdavies1983@gmail.com>
2020-05-05 19:27:14 +0100

    Tidy up

--- app.js
+++ app.js
@@ -33,13 +33,12 @@ function divide(a, b){
 
 function operate(operator, number1, number2) {
    
-  value = window[operator](number1,
-  number2);
+  value = window[operator](number1, number2);
   
   return value;
 }
 
-for(let i = 0; i < pageButton.length; i += 1) {
+for (let i = 0; i < pageButton.length; i += 1) {
 
   pageButton[i].addEventListener('click', (e) => { 
 
@@ -53,98 +52,55 @@ for(let i = 0; i < pageButton.length; i += 1) {
         inputValue1 = parseFloat(display.value);
      
       } else if (firstInput === false) {
+
           displayValue += pageButton[i].value; 
           display.value = displayValue;
           inputValue2 = parseFloat(display.value);
-        }
-   
-        decimal.disabled = false;
-   
-     } else if (pageButton[i].className === "operator" && firstPass === true ) {
-          
-          
-    operatorValue = pageButton[i].value;
-    displayValue = "";
-    firstInput = false;
-    newCalc = false;
-    firstPass = false;
-     
-  } 
-  
-  else if (pageButton[i].className === "operator" && firstPass === false) {
-      
-      
-      
-      calcValue = operate(operatorValue, inputValue1, inputValue2);
-      
-      operatorValue = pageButton[i].value;
-      
-      
-      display.value = calcValue;
-      
-      inputValue1 = calcValue;
-       
-      
-      
-       displayValue = "";
-       
-       
-       
-    
-     }
-      
-         
-   else if  (pageButton[i].className === "equal-sign") {
-   
-   
-   
-       calcValue =  operate(operatorValue, inputValue1, inputValue2);
-       
-      display.value = calcValue;
-      
-      inputValue1 = calcValue;
-      
-       displayValue = "";
-       
-       
-       
-     
-       
-       newCalc = true;
-       firstPass = true;
-       
-       
       }
-      
- 
-       
-       
-       
-         else if (pageButton[i].className === "all-clear"){
-         display.value = "0";
-         displayValue = "";
-         firstInput = true;
-         
-         inputValue1 = 0;
-         inputValue2 = 0;
-         firstPass = true;
-         }
-         
-         else if ( pageButton[i].className === "decimal"){
-         displayValue += pageButton[i].value;
-         decimal = pageButton[i];
-         
-        decimal.disabled = true; console.log(pageButton[i].value);
-         }
-         
-         
-         
+   
+      decimal.disabled = false;
+   
+    } else if (pageButton[i].className === "operator" && firstPass === true ) {
+          
+      operatorValue = pageButton[i].value;
+      displayValue = "";
+      firstInput = false;
+      newCalc = false;
+      firstPass = false;
      
+    } else if (pageButton[i].className === "operator" && firstPass === false) {
       
+        calcValue = operate(operatorValue, inputValue1, inputValue2);
       
+        operatorValue = pageButton[i].value;
+        display.value = calcValue;
+        inputValue1 = calcValue;
+        displayValue = "";
+    
+      } else if  (pageButton[i].className === "equal-sign") {
+   
+        calcValue =  operate(operatorValue, inputValue1, inputValue2);
+          
+        display.value = calcValue;
+        inputValue1 = calcValue;
+        displayValue = "";
+        newCalc = true;
+        firstPass = true;
        
-         });
-            
+      } else if (pageButton[i].className === "all-clear") {
+          display.value = "0";
+          displayValue = "";
+          firstInput = true;
+          inputValue1 = 0;
+          inputValue2 = 0;
+          firstPass = true;
+        
+      } else if ( pageButton[i].className === "decimal") {
+          displayValue += pageButton[i].value;
+          decimal = pageButton[i];
+          decimal.disabled = true; 
+      }
+  });
 }
     
         
